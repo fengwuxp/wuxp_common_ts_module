ac0a21f9acff24bc7cbd1b5ab0d5bb3f9870a138
风 <1109695647@qq.com>
2018-09-08 00:20:38 +0800

    Signed-off-by: 风 <1109695647@qq.com>

---
+++ packages/common_webpack/src/config/WebpackConfig.ts
@@ -0,0 +1,17 @@
+/**
+ *
+ * @author wxup
+ * @create 2018-09-08 0:14
+ **/
+function getWebpackConfig() {
+    if (process.env._self !== "1") {
+        return require("../../../../webpack-config/WebpackConfig");
+    }
+    return {};
+}
+
+export const {
+    DEPLOYMENT_DIRECTORY,
+    INCLUDE_PATH,
+    PROJECT_DIR
+} = getWebpackConfig();
