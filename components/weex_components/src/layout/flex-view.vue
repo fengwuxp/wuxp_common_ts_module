0a06d87693d7cc87f4f162bdddff6ec2d3f1868e
风 <1109695647@qq.com>
2018-09-08 12:24:12 +0800

    Signed-off-by: 风 <1109695647@qq.com>

---
+++ components/weex_components/src/layout/flex-view.vue
@@ -0,0 +1,14 @@
+<!--flex view-->
+<template>
+
+</template>
+
+<script>
+    export default {
+        name: "flex-view"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ components/weex_components/src/layout/header/app-base-header.vue
@@ -0,0 +1,14 @@
+<!--app base header -->
+<template>
+
+</template>
+
+<script>
+    export default {
+        name: "app-base-header"
+    }
+</script>
+
+<style scoped>
+
+</style>
