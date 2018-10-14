d6e3fca53cb5ee75baf79e1f278b8520caa9446f
风 <1109695647@qq.com>
2018-10-14 12:23:21 +0800

    Signed-off-by: 风 <1109695647@qq.com>

--- packages/common_fetch/src/decorator/FetchClient.ts
+++ packages/common_fetch/src/decorator/FetchClient.ts
@@ -2,10 +2,10 @@ import BuilderProxyServiceUtil from "../utils/BuilderProxyServiceUtil";
 
 
 /**
- * 标记为fetchService
+ * 标记为FetchClient
  * @constructor
  */
-export function FetchService<T extends { new(...args: any[]): any }>() {
+export function FetchClient<T extends { new(...args: any[]): any }>() {
 
     /**
      * @param  {T} clazz
