9e0b6f676926c24c6c1fa4ac71dd0e54ae7c0678
风 <1109695647@qq.com>
2018-10-22 10:31:33 +0800

    Signed-off-by: 风 <1109695647@qq.com>

--- packages/common_websocket/src/adapter/web/BrowserWebSocketAdapter.ts
+++ packages/common_websocket/src/adapter/web/BrowserWebSocketAdapter.ts
@@ -10,10 +10,18 @@ export default class BrowserWebSocketAdapter extends AbstractWebSocketAdapter<We
 
     constructor(options: WebSocketOptions, handler: WebSocketLifeCycleHandler) {
         super(new WebSocket(options.url, options.protocols), handler);
+
         this.socket.onclose = this.onClose;
         this.socket.onerror = this.onError;
         this.socket.onmessage = this.onMessage;
         this.socket.onopen = this.onOpen;
+
+        window.onbeforeunload = () => {
+            if (this.socket) {
+                //监听窗口关闭事件，当窗口关闭时，主动去关闭websocket连接，防止连接还没断开就关闭窗口，server端会抛异常
+                this.socket.close();
+            }
+        };
 
         return this;
     }
