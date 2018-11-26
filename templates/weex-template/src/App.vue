80fdeb116ba17b35729cf717d9f60330cb4a4aab
风 <1109695647@qq.com>
2018-11-26 16:05:33 +0800

    Signed-off-by: 风 <1109695647@qq.com>

--- templates/weex-template/src/App.vue
+++ templates/weex-template/src/App.vue
@@ -1,50 +1,19 @@
+<!--webd端 应用入口-->
 <template>
-    <flex-view>
-        <nav-bar slot="app-header" :navTitle="navTitle"></nav-bar>
-        <div slot="app-body" class="flex_1"></div>
-        <div slot="app-footer" class="app_footer"></div>
-    </flex-view>
+    <div @androidback="back">
+        <keep-alive>
+            <router-view style="flex:1" v-if="$route.meta.keepAlive"></router-view>
+        </keep-alive>
+        <router-view style="flex:1" v-if="!$route.meta.keepAlive"></router-view>
+    </div>
 </template>
 
 <script>
-
-
-    import AppMixin from "weex_starter/src/mixins/AppMixin"
-    import FlexView from "weex_components/src/layout/view/flex-view";
-    import NavBar from "weex_components/src/layout/navbar/nav-bar";
-
     export default {
-        components: {NavBar, FlexView},
-        mixins: [AppMixin],
-        props: {
-            navTitle: {
-                default: "测试页面"
+        methods: {
+            back: function () {
+                this.$router.back()
             }
-        },
-        data() {
-            return {};
-        },
-        methods: {},
-        beforeMount() {
-
         }
     }
 </script>
-<style scoped lang="less">
-
-    @import "./less/theme";
-
-    .text_red {
-        color: @primary-color;
-    }
-
-    .flex_1 {
-        flex: 1;
-        background-color: @fill-body;
-    }
-
-    .app_footer {
-        height: 120px;
-        background-color: @primary-color;
-    }
-</style>
