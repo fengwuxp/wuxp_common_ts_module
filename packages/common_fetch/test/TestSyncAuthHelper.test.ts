517b0d223027cd3f99e4a7c21209aadc617334d7
风 <1109695647@qq.com>
2018-12-08 14:55:15 +0800

    Signed-off-by: 风 <1109695647@qq.com>

--- packages/common_fetch/test/TestSyncAuthHelper.test.ts
+++ packages/common_fetch/test/TestSyncAuthHelper.test.ts
@@ -31,10 +31,12 @@ class LockExecutor {
             return Promise.resolve(this.member);
         }
 
+        //token已经失效
         logger.warn("token 失效，刷新token", this.member.token);
 
         this.count++;
         if (this.lockTask == null) {
+            //异步任务去获取用户的鉴权信息
             this.lockTask = new Promise<Member>((resolve, reject) => {
                 logger.info("开始刷新token");
                 //模拟网络请求
@@ -43,13 +45,13 @@ class LockExecutor {
                         id: Math.random() * 200,
                         token: Math.random() * 200 + ""
                     })
-                }, 3 * 1000)
+                }, 3 * 1000);
             });
         }
 
         return new Promise((resolve, reject) => {
-            logger.debug("等待锁释放->");
-            //等待锁释放
+            logger.debug("等待异步任务的完成->");
+            //等待异步任务的完成
             this.lockTask.then((data) => {
                 resolve(data);
                 this.count--;
@@ -76,6 +78,7 @@ class TestSyncAuthHelper implements SyncAuthHelper<Member> {
 
     async requestParamsEnhance(params: FetchOptions): Promise<boolean> {
 
+        //获取鉴权信息
         params.data = await this.lockExecute();
         return true;
     };
@@ -91,6 +94,7 @@ describe("sync auth helper test", () => {
     test("auth test", async () => {
         const time = new Date().getTime();
         logger.debug("开始时间", time);
+        let count = 0;
         const options = [
             {
                 url: "0"
@@ -108,27 +112,44 @@ describe("sync auth helper test", () => {
                 url: "4"
             }
         ];
-        syncAuthHelper.requestParamsEnhance(options[0] as any).then(() => {
-            logger.debug("options[0]", options[0]);
+
+        const complete = (resolve) => {
+            count++;
+            if (count === options.length) {
+                resolve();
+            }
+        };
+        return new Promise((resolve, reject) => {
+
+
+            syncAuthHelper.requestParamsEnhance(options[0] as any).then(() => {
+                logger.debug("options[0]", options[0]);
+                complete(resolve);
+            });
+
+            syncAuthHelper.requestParamsEnhance(options[1] as any).then(() => {
+                logger.debug("options[1]", options[1]);
+                complete(resolve);
+            });
+
+            syncAuthHelper.requestParamsEnhance(options[2] as any).then(() => {
+                logger.debug("options[2]", options[2]);
+                complete(resolve);
+            });
+
+            syncAuthHelper.requestParamsEnhance(options[3] as any).then(() => {
+                logger.debug("options[3]", options[3]);
+                complete(resolve);
+            });
+
+            syncAuthHelper.requestParamsEnhance(options[4] as any).then(() => {
+                logger.debug("options[4]", options[4]);
+                complete(resolve);
+            });
+
         });
 
-        syncAuthHelper.requestParamsEnhance(options[1] as any).then(() => {
-            logger.debug("options[1]", options[1]);
-        });
-
-        syncAuthHelper.requestParamsEnhance(options[2] as any).then(() => {
-            logger.debug("options[2]", options[2]);
-        });
-
-        syncAuthHelper.requestParamsEnhance(options[3] as any).then(() => {
-            logger.debug("options[3]", options[3]);
-        });
-
-        await syncAuthHelper.requestParamsEnhance(options[4] as any).then(() => {
-            logger.debug("options[4]", options[4]);
-        });
-
-        let endTime = new Date().getTime();
+        const endTime = new Date().getTime();
         logger.debug("结束时间", endTime);
         logger.debug("耗时", (endTime - time) / 1000);
 
