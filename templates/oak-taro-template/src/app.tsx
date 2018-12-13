0f762744a917c6c8fd444d6725f54631db1d505b
风 <1109695647@qq.com>
2018-12-13 14:36:01 +0800

    Signed-off-by: 风 <1109695647@qq.com>

---
+++ templates/oak-taro-template/src/app.tsx
@@ -0,0 +1,96 @@
+import Taro, {Component, Config} from '@tarojs/taro'
+import {Provider} from '@tarojs/mobx'
+import Index from './pages/index'
+
+import counterStore from './store/counter'
+
+import './app.less'
+
+// 如果需要在 h5 环境中开启 React Devtools
+// 取消以下注释：
+// if (process.env.NODE_ENV !== 'production' && process.env.TARO_ENV === 'h5')  {
+//   require('nerv-devtools')
+// }
+
+const store = {
+  counterStore
+};
+
+class App extends Component {
+
+  /**
+   * 指定config的类型声明为: Taro.Config
+   *
+   * 由于 typescript 对于 object 类型推导只能推出 Key 的基本类型
+   * 对于像 navigationBarTextStyle: 'black' 这样的推导出的类型是 string
+   * 提示和声明 navigationBarTextStyle: 'black' | 'white' 类型冲突, 需要显示声明类型
+   */
+  config: Config = {
+    pages: [
+      'pages/index/index'
+    ],
+    window: {
+      backgroundTextStyle: 'light',
+      navigationBarBackgroundColor: '#fff',
+      navigationBarTitleText: 'WeChat',
+      navigationBarTextStyle: 'black'
+    },
+
+    tabBar: {
+      list: [
+        {
+          pagePath: "pages/home/index",
+          text: "首页",
+          iconPath: "../static_resources/images/tab/home.png",
+          selectedIconPath: "../static_resources/images/tab/home-active.png"
+        },
+
+        {
+          pagePath: "pages/cart/index",
+          text: "衣袋",
+          iconPath: "../static_resources/images/tab/cart.png",
+          selectedIconPath: "../static_resources/images/tab/cart-active.png"
+        },
+        {
+          pagePath: "pages/cart/index",
+          text: "衣袋",
+          iconPath: "../static_resources/images/tab/cart.png",
+          selectedIconPath: "../static_resources/images/tab/cart-active.png"
+        },
+        {
+          pagePath: "pages/user/index",
+          text: "我的",
+          iconPath: "../static_resources/images/tab/user.png",
+          selectedIconPath: "../static_resources/images/tab/user-active.png"
+        }
+      ],
+      color: '#333333',
+      selectedColor: '#333333',
+      backgroundColor: '#ffffff',
+    }
+  };
+
+  componentDidMount() {
+  }
+
+  componentDidShow() {
+  }
+
+  componentDidHide() {
+  }
+
+  componentDidCatchError() {
+  }
+
+  // 在 App 类中的 render() 函数没有实际作用
+  // 请勿修改此函数
+  render() {
+    return (
+      <Provider store={store}>
+        <Index/>
+      </Provider>
+    )
+  }
+}
+
+Taro.render(<App/>, document.getElementById('app'))
---
+++ templates/oak-taro-template/src/pages/index/index.tsx
@@ -0,0 +1,77 @@
+import Taro, {Component, Config} from '@tarojs/taro'
+import {View, Button, Text} from '@tarojs/components'
+import {observer, inject} from '@tarojs/mobx'
+
+import './index.less'
+
+
+interface IndexProps {
+    counterStore?: any
+}
+
+@inject('counterStore')
+@observer
+class Index extends Component<IndexProps, {}> {
+
+    /**
+     * 指定config的类型声明为: Taro.Config
+     *
+     * 由于 typescript 对于 object 类型推导只能推出 Key 的基本类型
+     * 对于像 navigationBarTextStyle: 'black' 这样的推导出的类型是 string
+     * 提示和声明 navigationBarTextStyle: 'black' | 'white' 类型冲突, 需要显示声明类型
+     */
+    config: Config = {
+        navigationBarTitleText: '首页'
+    };
+
+    componentWillMount() {
+    }
+
+    componentWillReact() {
+        console.log('componentWillReact')
+    }
+
+    componentDidMount() {
+
+        Taro.navigateBack()
+    }
+
+    componentWillUnmount() {
+    }
+
+    componentDidShow() {
+
+    }
+
+    componentDidHide() {
+    }
+
+    increment = () => {
+        const {counterStore} = this.props;
+        counterStore.increment()
+    }
+
+    decrement = () => {
+        const {counterStore} = this.props;
+        counterStore.decrement()
+    }
+
+    incrementAsync = () => {
+        const {counterStore} = this.props;
+        counterStore.incrementAsync()
+    }
+
+    render() {
+        const {counterStore: {counter}} = this.props
+        return (
+            <View className='index'>
+                <Button onClick={this.increment}>+</Button>
+                <Button onClick={this.decrement}>-</Button>
+                <Button onClick={this.incrementAsync}>Add Async</Button>
+                <Text>{counter}</Text>
+            </View>
+        )
+    }
+}
+
+export default Index
