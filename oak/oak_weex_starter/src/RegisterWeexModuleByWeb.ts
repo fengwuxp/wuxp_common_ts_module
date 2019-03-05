3eeee5d1c7ee05274e3ebba4359d4254090370ec
风 <1109695647@qq.com>
2019-03-05 19:02:17 +0800

    Signed-off-by: 风 <1109695647@qq.com>

--- oak/oak_weex_starter/src/RegisterWeexModuleByWeb.ts
+++ oak/oak_weex_starter/src/RegisterWeexModuleByWeb.ts
@@ -1,19 +1,20 @@
 /**
- * 自定义weex模块，需要h5 android ios分别做实现
+ * 注册（导入）自定义weex模块
  */
-
 export const registerWeexModuleByWeb = () => {
     require("./module/boradcast/Broadcast");
+    require("./module/push/MessagagePush");
+    require("./module/scan/Qrcode");
+    require("./module/version/AppUpdate");
+    require("./module/location/Location");
+    require("./module/location/NaviMapModule");
+    require("./module/third/ThirdLogin");
+    require("./module/pay/ALiPayModule");
+    require("./module/picker/DateTimerPickerModule");
+
     require("./module/Cache");
     require("./module/AppMain");
     require("./module/ImageLoad");
-    require("./module/push/MessagagePush");
-    require("./module/scan/Qrcode");
     require("./module/Common");
-    require("./module/version/AppUpdate");
     require("./module/Photo");
-    require("./module/location/Location");
-    require("./module/third/ThirdLogin");
-    require("./module/pay/ALiPayModule");
-    require("./module/location/NaviMapModule");
 };
