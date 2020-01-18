060c50e23edcd72ccdb206a0441352a221aafcb8
风 <1109695647@qq.com>
2020-01-18 15:33:12 +0800

    Signed-off-by: 风 <1109695647@qq.com>

--- oak/oak_common_2.x/rollup.config.js
+++ oak/oak_common_2.x/rollup.config.js
@@ -1,15 +1,13 @@
 import * as os from 'os';
-import * as  path from "path";
 import resolve from 'rollup-plugin-node-resolve';
 import common from 'rollup-plugin-commonjs';
-import babel from 'rollup-plugin-babel';
 import {terser} from 'rollup-plugin-terser';
 import typescript from 'rollup-plugin-typescript2';
 import json from 'rollup-plugin-json';
-import dts from "rollup-plugin-dts";
 import filesize from "rollup-plugin-filesize";
 import includePaths from "rollup-plugin-includepaths";
 import analyze from "rollup-plugin-analyzer";
+import dts from "rollup-plugin-dts";
 
 import pkg from './package.json';
 import {DEFAULT_EXTENSIONS} from "@babel/core";
@@ -33,14 +31,6 @@ const getConfig = (isProd) => {
         output: [
             {
                 file: isProd ? pkg.main.replace(".js", ".min.js") : pkg.main,
-                format: 'commonjs',
-                compact: true,
-                extend: false,
-                sourcemap: isProd,
-                strictDeprecations: false
-            },
-            {
-                file: isProd ? pkg.module.replace(".js", ".min.js") : pkg.module,
                 format: 'esm',
                 compact: true,
                 extend: false,
@@ -68,11 +58,6 @@ const getConfig = (isProd) => {
                 // 排除
                 exclude: [],
                 extensions: [...DEFAULT_EXTENSIONS, ".ts", ".tsx"],
-            }),
-            babel({
-                exclude: "node_modules/**",
-                babelHelpers: "runtime",
-                extensions: [...DEFAULT_EXTENSIONS, ".ts", ".tsx"]
             }),
             analyze({
                 stdout: true,
